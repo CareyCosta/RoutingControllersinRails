2b53250d0b8f807dde6b2790ea9d444dc7693bfc
Carey Costa <careyxcosta@gmail.com>
2017-01-30 13:53:58 -0500

    kitten(S), set_kitten_url

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ get '/welcome' => 'pages#welcome'
 get '/' => 'pages#welcome'
 get '/about' => 'pages#about'
 get '/contest' => 'pages#contest'
+get '/kitten/:size' => 'pages#kitten'
+get '/kittens/:size' => 'pages#kitten'
 
 end
